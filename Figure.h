4cb29a2d0d65f4b8758ce17fb0a98121a2f32e0b
konnata14 <n4koli@yahoo.com>
2025-11-14 21:51:53 +0300

    Update Figure.h

--- Figure.h
+++ Figure.h
@@ -1,50 +1,162 @@
-//Заголовочный файл класса Figure (Figure.h)
+// Figure.h
 #pragma once
-#include <windows.h>
-#include <gdiplus.h>
+
+#include <stdexcept>
+#include <cmath>
 
 using namespace System;
 using namespace System::Drawing;
 
-public ref class Figure abstract {
-protected:
-    int radius;
-    int thickness;
-    Point center;
-    Color borderColor;
-    Color fillColor;
+namespace WindowsFormsApp1 {
 
-public:
-    Figure(Point center, int radius, int thickness, Color borderColor, Color fillColor)
-        : center(center), radius(radius), thickness(thickness),
-        borderColor(borderColor), fillColor(fillColor) {
-    }
+    public ref class BorderException : public Exception {
+    public:
+        BorderException(String^ message) : Exception(message) {}
+    };
 
-    virtual void Draw(Graphics^ g) abstract;
+    public ref class NegativeException : public Exception {
+    public:
+        NegativeException(String^ message) : Exception(message) {}
+    };
 
-    virtual void Hide(Graphics^ g) {
-        Color oldBorder = borderColor;
-        Color oldFill = fillColor;
-        borderColor = Color::Transparent;
-        fillColor = Color::Transparent;
-        Draw(g);
-        borderColor = oldBorder;
-        fillColor = oldFill;
-    }
+    public ref class Figure abstract {
+    protected:
+        int r;
+        int a;
+        Point center;
+        Color color1;
+        Color color2;
 
-    virtual void MoveTo(Point newCenter, Graphics^ g) {
-        Hide(g);
-        center = newCenter;
-        Draw(g);
-    }
+    public:
+        Figure(Point t, int r, int a, Color c1, Color c2)
+            : center(t), r(r), a(a), color1(c1), color2(c2) {
+        }
 
-    property Point Center {
-        Point get() { return center; }
-        void set(Point value) { center = value; }
-    }
+        virtual void Draw(Graphics^ g) = 0;
 
-    property int Radius {
-        int get() { return radius; }
-        void set(int value) { radius = value; }
-    }
+        virtual void Hide(Graphics^ g) {
+            // Temporary draw with background-like color (use White to actually hide on white background)
+            Color save1 = color1;
+            Color save2 = color2;
+            color1 = Color::White;
+            color2 = Color::White;
+            Draw(g);
+            color1 = save1;
+            color2 = save2;
+        }
 
+        virtual void Replace(Point t) {
+            center = t;
+        }
+
+        virtual void SetBorderColor(Color color) {
+            color1 = color;
+        }
+
+        virtual void SetFillColor(Color color) {
+            color2 = color;
+        }
+
+        property Point Center{
+            Point get() { return center; }
+        }
+
+            property int Radius{
+                int get() { return r; }
+        }
+    };
+
+    public ref class Circle : public Figure {
+    public:
+        Circle(Point t, int r, int a, Color c1, Color c2) : Figure(t, r, a, c1, c2) {}
+
+        virtual void Draw(Graphics^ g) override {
+            if (center.X < 0 || center.Y < 0) {
+                throw gcnew NegativeException("Circle has negative coordinates.");
+            }
+            if (r < 0) {
+                throw gcnew NegativeException("Circle has negative radius.");
+            }
+            // VisibleClipBounds can return float, use panel size checking earlier,
+            // but still check approximate bounds here
+            if (center.X - r < 0 || center.Y - r < 0) {
+                throw gcnew BorderException("Circle is out of left/top border.");
+            }
+            Pen^ pen = gcnew Pen(color1, 4);
+            SolidBrush^ brush = gcnew SolidBrush(color2);
+            g->DrawEllipse(pen, center.X - r, center.Y - r, r * 2, r * 2);
+            g->FillEllipse(brush, center.X - r, center.Y - r, r * 2, r * 2);
+            delete pen;
+            delete brush;
+        }
+    };
+
+    public ref class Octagon : public Figure {
+    public:
+        Octagon(Point t, int r, int a, Color c1, Color c2) : Figure(t, r, a, c1, c2) {}
+
+        virtual void Draw(Graphics^ g) override {
+            if (center.X < 0 || center.Y < 0) {
+                throw gcnew NegativeException("Octagon has negative coordinates.");
+            }
+            if (r < 0) {
+                throw gcnew NegativeException("Octagon has negative radius.");
+            }
+            array<PointF>^ points = gcnew array<PointF>(8);
+            double angle = -M_PI / 8.0; // rotate a bit for nicer orientation
+            for (int i = 0; i < 8; ++i) {
+                points[i].X = center.X + r * (float)cos(angle);
+                points[i].Y = center.Y + r * (float)sin(angle);
+                angle += 2 * M_PI / 8.0;
+            }
+            Pen^ pen = gcnew Pen(color1, 4);
+            SolidBrush^ brush = gcnew SolidBrush(color2);
+            g->DrawPolygon(pen, points);
+            g->FillPolygon(brush, points);
+            delete pen;
+            delete brush;
+        }
+    };
+
+    public ref class ComFigure : public Figure {
+    private:
+        Circle^ circle;
+        Octagon^ octagon;
+    public:
+        ComFigure(Point t, int r, int a, Color c1, Color c2, Color c3, Color c4)
+            : Figure(t, r, a, c1, c2)
+        {
+            circle = gcnew Circle(t, r, a, c1, c2);
+            octagon = gcnew Octagon(t, r, a, c3, c4);
+        }
+
+        virtual void Draw(Graphics^ g) override {
+            circle->Draw(g);
+            octagon->Draw(g);
+        }
+
+        virtual void Hide(Graphics^ g) override {
+            circle->Hide(g);
+            octagon->Hide(g);
+        }
+
+        virtual void Replace(Point t) override {
+            circle->Replace(t);
+            octagon->Replace(t);
+            Figure::Replace(t);
+        }
+
+        virtual void SetBorderColor(Color color) override {
+            circle->SetBorderColor(color);
+            octagon->SetBorderColor(color);
+            Figure::SetBorderColor(color);
+        }
+
+        virtual void SetFillColor(Color color) override {
+            circle->SetFillColor(color);
+            octagon->SetFillColor(color);
+            Figure::SetFillColor(color);
+        }
+    };
+
+} // namespace WindowsFormsApp1
